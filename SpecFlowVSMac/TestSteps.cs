a8335ae22d30a84639b14996c458ac4f668841d9
Cecil G. Williams <cecil.g.williams@gmail.com>
2018-04-03 14:20:30 -0500

    SpecFlow project using Visual Studio for Mac
    Used https://github.com/straighteight/SpecFlow-VS-Mac-Integration and created new SpecFlow NUnit Library Project

---
+++ SpecFlowVSMac/TestSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowVSMac
+{
+    [Binding]
+    public class TestSteps
+    {
+        private List<int> _numbers = new List<int>();
+        private int _sum;
+
+        [Given("I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredSomethingIntoTheCalculator(int number)
+        {
+            _numbers.Add(number);
+        }
+
+        [When("I press add")]
+        public void WhenIPressAdd()
+        {
+            _sum = _numbers.Sum();
+        }
+
+        [Then("the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            Assert.AreEqual(result, _sum);
+        }
+    }
+}
